6a15b9dfcadde6917d919a249bd56dc16565b44a
toripotts <vcpotts@gmail.com>
2020-01-24 08:57:21 -0500

    progress in build

--- js/main.js
+++ js/main.js
@@ -1,3 +1,102 @@
+// const number1 = document.querySelector("#number1")
+// const number2 = document.querySelector("#number2")
+// const number3 = document.querySelector("#number3")
+// const number4 = document.querySelector("#number4")
+// const number5 = document.querySelector("#number5")
+// const number6 = document.querySelector("#number6")
+// const number7 = document.querySelector("#number7")
+// const number8 = document.querySelector("#number8")
+// const number9 = document.querySelector("#number9")
+// const number0 = document.querySelector("#number0")
+const clear = document.querySelector("#clearBtn");
+const display = document.querySelector("#screen");
+const divideBtn = document.querySelector("#divideBtn");
+const multiplyBtn = document.querySelector("#multiplyBtn");
+const addBtn = document.querySelector("#addBtn");
+const screen = document.querySelector("#screen");
+
+divideBtn.addEventListener("click", function() {
+  clearScreen();
+});
+
+multiplyBtn.addEventListener("click", function() {
+  clearScreen();
+});
+
+addBtn.addEventListener("click", function() {
+  clearScreen();
+});
+
+subtractBtn.addEventListener("click", function() {
+  clearScreen();
+});
+
+const buttonObj = {};
+for (let i = 0; i < 10; i++) {
+  buttonObj[`number${i}`] = document.querySelector(`#number${i}`);
+  buttonObj[`number${i}`].addEventListener("click", function() {
+    updateNumbers(i);
+    renderScreenOnKeyDown();
+  });
+}
+
+function renderScreenOnKeyDown() {
+  let textInput = numbersSelected.join("");
+  screen.textContent = textInput;
+  return textInput;
+}
+
+function clearScreen() {
+  screen.textContent = "";
+}
+
+let numbersSelected = [];
+
+function clearNumbersSelected() {
+  numbersSelected.length = 0;
+  return numbersSelected;
+}
+
+function updateNumbers(number) {
+  numbersSelected.push(number);
+  return numbersSelected;
+}
+clear.addEventListener("click", function() {
+  clearNumbersSelected();
+  clearScreen();
+  console.log(numbersSelected);
+});
+// number1.addEventListener("click", function(){
+//     updateNumbers(1);
+// });
+// number2.addEventListener("click", function() {
+//     updateNumbers(2)
+// });
+// number3.addEventListener("click", function() {
+//     updateNumbers(3)
+// });
+// number4.addEventListener("click", function() {
+//     updateNumbers(4)
+// });
+// number5.addEventListener("click", function() {
+//     updateNumbers(5)
+// });
+// number6.addEventListener("click", function() {
+//     updateNumbers(6)
+// });
+// number7.addEventListener("click", function() {
+//     updateNumbers(7)
+// });
+// number8.addEventListener("click", function() {
+//     updateNumbers(8)
+// });
+// number9.addEventListener("click", function() {
+//     updateNumbers(9)
+// });
+// number0.addEventListener("click", function() {
+//     updateNumbers(0)
+// });
+
 function add(x, y) {
   let sum = x + y;
   return sum;
@@ -27,5 +126,7 @@ function operator(operand, num1, num2) {
     return multiply(num1, num2);
   } else if (operand == divide) {
     return divide(num1, num2);
-  } else {return "ERROR";}
+  } else {
+    return "ERROR";
+  }
 }
